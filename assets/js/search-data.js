c1d798e26255dee7458eeb547f53dffc56c3dd8b
sagihaider <sagihaider@gmail.com>
2024-12-28 22:31:37 +0000

    Deploying to gh-pages from @ sagihaider/sagihaider.github.io@44e6536cbc74e766452517a0319a951e0a62bd8d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -502,13 +502,6 @@ ninja.data = [{
           window.open("https://github.com/sagihaider", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
