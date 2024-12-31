1f1c14fd4ca1e970c0df6f35ed1f6b2d8ebeac6a
sagihaider <sagihaider@gmail.com>
2024-12-31 14:54:18 +0000

    Deploying to gh-pages from @ sagihaider/sagihaider.github.io@348c88d8c3ffd31c3a7af444d92dba5e902aca31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,6 +51,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "Members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
