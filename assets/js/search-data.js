e156a85c57f419d1d133b68ff07552ba47da6d40
sagihaider <sagihaider@gmail.com>
2026-08-18 14:11:22 +0000

    Deploying to gh-pages from @ sagihaider/sagihaider.github.io@e54f524ddbc2d46111b9d08204333b212a41d529 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A collection of ongoing and sucessfully completed funded projects.",
+          description: "A collection of ongoing and successfully completed funded projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
